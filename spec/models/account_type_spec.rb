bcdbfd16aed01384d3b5d1a14a5c8043fedca494
Paul BH <eyefodder@gmail.com>
2014-07-11 00:00:25 -0400

    fixed strange shoulda issue

--- spec/models/account_type_spec.rb
+++ spec/models/account_type_spec.rb
@@ -27,10 +27,10 @@ end
 
 describe AccountType do
   let(:account_type){build(:account_type)}
-  xit 'should require a name' do
+  it 'should require a name' do
     expect(account_type).to validate_presence_of(:name_id)
   end
-  xit 'should require a unique name' do
+  it 'should require a unique name' do
     expect(account_type).to validate_uniqueness_of(:name_id)
   end
 
