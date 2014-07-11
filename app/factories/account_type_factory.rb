ddfc47a6d8b299948c00f7a3a40bbf8890692bb1
Paul BH <eyefodder@gmail.com>
2014-07-11 00:58:27 -0400

    part refactor of factory

--- app/factories/account_type_factory.rb
+++ app/factories/account_type_factory.rb
@@ -3,34 +3,45 @@ class AccountTypeFactory
   def self.account_type_for(user)
     # NB explicitly testing against 'false' as we want to avoid nil getting caught in false check
     # ie there is a diff between user.has_predictable_income = false and user.has_predictable_income  = nil!
-    if user.is_delinquent
-      if user.has_predictable_income == false
-        return AccountType.PREPAY_CARD
-      elsif user.has_predictable_income
-        return user.in_new_york_city? ? AccountType.SAFE_ACCOUNT : AccountType.SECOND_CHANCE
-      end
-    elsif user.is_delinquent == false
-      if user.is_special_group
-        return AccountType.SPECIAL_GROUP
-      elsif user.is_special_group == false
-        if user.will_use_direct_deposit
-          return AccountType.REGULAR_ACCOUNT
-        elsif user.will_use_direct_deposit == false
-          if !user.in_new_york_city?
-            return AccountType.CREDIT_UNION
-          else
-            if user.needs_debit_card == false
-              return AccountType.SAFE_ACCOUNT
-            elsif user.needs_debit_card
-              return AccountType.CREDIT_UNION
-            end
-          end
-        end
 
+    if matches_prepay_profile(user)
+      return AccountType.PREPAY_CARD
+    end
+    if matches_safe_or_second_chance(user)
+      return safe_or_second_chance(user)
+    end
+    if matches_special_group_profile(user)
+      return AccountType.SPECIAL_GROUP
+    end
+    if matches_regular_account_profile(user)
+      return AccountType.REGULAR_ACCOUNT
+    end
+    if user.is_delinquent == false && user.is_special_group == false && user.will_use_direct_deposit == false
+      if !user.in_new_york_city?
+        return AccountType.CREDIT_UNION
+      else
+        if user.needs_debit_card == false
+          return AccountType.SAFE_ACCOUNT
+        elsif user.needs_debit_card
+          return AccountType.CREDIT_UNION
+        end
       end
     end
+  end
 
-
-
+  def self.safe_or_second_chance(user)
+    user.in_new_york_city? ? AccountType.SAFE_ACCOUNT : AccountType.SECOND_CHANCE
+  end
+  def self.matches_safe_or_second_chance(user)
+    user.is_delinquent && user.has_predictable_income
+  end
+  def self.matches_prepay_profile(user)
+    user.is_delinquent && user.has_predictable_income == false
+  end
+  def self.matches_special_group_profile(user)
+    user.is_delinquent == false && user.is_special_group
+  end
+  def self.matches_regular_account_profile(user)
+user.is_delinquent == false && user.is_special_group == false && user.will_use_direct_deposit
   end
 end
