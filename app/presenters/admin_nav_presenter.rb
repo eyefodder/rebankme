f69686a12ef328325510993b9b9a416cf7f8e292
Paul BH <eyefodder@gmail.com>
2014-07-14 20:13:49 -0400

    tidied admin nav presenter

---
+++ app/presenters/admin_nav_presenter.rb
@@ -0,0 +1,33 @@
+class AdminNavPresenter < BasePresenter
+
+
+  def admin_home_link
+    link_for('Admin Home', h.admin_path)
+  end
+  def banks_link
+    link_for('Banks', h.banks_path)
+  end
+
+  def edit_account_link
+    link_for('Edit Account', h.edit_admin_user_registration_path)
+  end
+
+  def logout_link
+    link = h.link_to('Log out', h.destroy_admin_user_session_path, method: :delete, id: 'logout')
+    h.content_tag(:li, link)
+  end
+
+
+  private
+
+  def link_for(copy, path)
+    h.content_tag(:li, h.link_to(copy, path), class: nav_link_class(path))
+  end
+
+  def nav_link_class(path)
+    h.request.fullpath == path ? 'active' : ''
+  end
+
+
+
+end
