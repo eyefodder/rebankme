fc152aa7e33e7a583b376acc34776a9c8de7ff08
Paul BH <eyefodder@gmail.com>
2014-07-15 21:47:57 -0400

    added quotaguard proxy

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,5 @@
+Geocoder.configure(
+  #ENV['QUOTAGUARD_URL'] only set on heroku, and not stored in source. When nil, no proxy will be used
+  :http_proxy => ENV['QUOTAGUARD_URL'],
+  :timeout => 5
+)
