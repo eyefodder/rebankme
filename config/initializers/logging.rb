3daaf96b0d81858944cb3decb3585c11e3e9fe0d
Paul BH <eyefodder@gmail.com>
2014-07-28 15:18:37 -0400

    logging in production

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,3 +1,3 @@
 Logging.logger.root.add_appenders(Logging.appenders.stdout)
-Logging.logger.root.add_appenders(Logging.appenders.file("/app/log/#{Rails.env}.log")) unless Rails.env.test?
+Logging.logger.root.add_appenders(Logging.appenders.file("/app/log/#{Rails.env}.log")) if Rails.env.development?
 Logging.logger.root.info {"\n\n\nSTARTING NEW LOGGING SESSION"}
