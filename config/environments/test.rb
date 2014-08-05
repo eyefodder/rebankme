1d8f48c00c6423b99043e73d927ea6f950a46057
Paul BH <eyefodder@gmail.com>
2014-08-05 13:03:51 -0400

    fixed broken build with default envirnment stuff for testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,15 @@ Rebankme::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.smtp_settings = {
+        address:              'smtp.example.com',
+        port:                 587,
+        domain:               'example.com',
+        user_name:            'username@example.com',
+        password:             'password',
+        authentication:       'plain',
+        enable_starttls_auto: true  }
+
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
