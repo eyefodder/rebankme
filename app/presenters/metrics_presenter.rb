c5fdb09a39204cd2f597f64786c06cd2fbd6a390
Paul BH <eyefodder@gmail.com>
2014-08-08 17:56:11 -0400

    quick fix

--- app/presenters/metrics_presenter.rb
+++ app/presenters/metrics_presenter.rb
@@ -3,25 +3,27 @@ class MetricsPresenter < BasePresenter
 
   def metrics_table
 
-    h.content_tag(:table, class: 'table table-striped table-condensed') do
-      head = h.content_tag(:thead) do
-        h.content_tag(:tr) do
-          h.content_tag(:th, "stage") +
-          h.content_tag(:th, "# ppl") +
-          h.content_tag(:th, "% of total") +
-          h.content_tag(:th, "% drop from last stage")
+    if metrics.length > 1
+
+      h.content_tag(:table, class: 'table table-striped table-condensed') do
+        head = h.content_tag(:thead) do
+          h.content_tag(:tr) do
+            h.content_tag(:th, "Stage") +
+            h.content_tag(:th, "# people") +
+            h.content_tag(:th, "% Of total") +
+            h.content_tag(:th, "% Drop from last stage")
+          end
         end
-      end
-      body = h.content_tag(:tbody) do
-        rows = ""
-        total = metrics.first[:total]
-        last = metrics.first[:total]
-        metrics.each do |metric_obj|
-          row = h.content_tag(:tr) do
-            amt = metric_obj[:total].to_f
-            pc_total = h.number_to_percentage((amt/total) * 100, precision: 1)
-            pc_from_last = h.number_to_percentage((amt/last) * 100, precision: 1)
-            last = amt
+        body = h.content_tag(:tbody) do
+          rows = ""
+          total = metrics.first[:total]
+          last = metrics.first[:total]
+          metrics.each do |metric_obj|
+            row = h.content_tag(:tr) do
+              amt = metric_obj[:total].to_f
+              pc_total = h.number_to_percentage((amt/total) * 100, precision: 1)
+              pc_from_last = h.number_to_percentage((amt/last) * 100, precision: 1)
+              last = amt
             # puts ("amt: #{amt}, total: #{total}")
 
             h.content_tag(:td, metric_obj[:metric].name) +
@@ -43,49 +45,51 @@ class MetricsPresenter < BasePresenter
       head + body
     end
 
-
-
   end
 
-  def funnel_chart
-    options =
-    {series:
-      { funnel:
-        {
+
+
+end
+
+def funnel_chart
+  options =
+  {series:
+    { funnel:
+      {
+        show: true,
+
+        margin: { right: 0.15},
+        label: {
           show: true,
+          align: "center",
+          threshold: 0.05,
 
-          margin: { right: 0.15},
-          label: {
-            show: true,
-            align: "center",
-            threshold: 0.05,
-
-            },
-            highlight: {
-              opacity: 0.2
-            }
-            },
-            },
-            grid: {
-              hoverable: true,
-              clickable: true
-            }
+          },
+          highlight: {
+            opacity: 0.2
           }
+          },
+          },
+          grid: {
+            hoverable: true,
+            clickable: true
+          }
+        }
 
-          data = []
-          metrics.each do |metric_obj|
-            data << {label: metric_obj[:metric].name, data:metric_obj[:total] }
-          end
-
-
-          h.content_tag(:div, "", id: 'funnel-chart', style: 'height:800px; width:800px', data: {chart_data: data, chart_options:options})
+        data = []
+        metrics.each do |metric_obj|
+          data << {label: metric_obj[:metric].name, data:metric_obj[:total] }
         end
 
 
-
-
-
-        private
-
-
+        h.content_tag(:div, "", id: 'funnel-chart', style: 'height:800px; width:800px', data: {chart_data: data, chart_options:options})
       end
+
+
+
+
+
+      private
+
+
+    end
