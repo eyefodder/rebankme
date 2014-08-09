857a461206e060d8c466137987a0ebb276db9da7
Paul BH <eyefodder@gmail.com>
2014-08-09 16:49:21 -0400

    tidied metric presenter

--- app/presenters/metrics_presenter.rb
+++ app/presenters/metrics_presenter.rb
@@ -1,83 +1,36 @@
 class MetricsPresenter < BasePresenter
   presents :metrics
 
-  def metrics_table
-    if metrics.length > 0
+  def metrics_new
+    h.render(partial: 'admin/metrics_table', locals: { presenter: self}) if metrics.length > 0
+  end
 
-      h.content_tag(:table, class: 'table table-striped table-condensed') do
-        head = h.content_tag(:thead) do
-          h.content_tag(:tr) do
-            h.content_tag(:th, "stage") +
-            h.content_tag(:th, "# ppl") +
-            h.content_tag(:th, "% of total") +
-            h.content_tag(:th, "% drop from last stage")
-          end
-        end
-        body = h.content_tag(:tbody) do
-          rows = ""
-          total = metrics.first[:total]
-          last = metrics.first[:total]
-          metrics.each do |metric_obj|
-            row = h.content_tag(:tr) do
-              amt = metric_obj[:total].to_f
-              pc_total = h.number_to_percentage((amt/total) * 100, precision: 1)
-              pc_from_last = h.number_to_percentage((amt/last) * 100, precision: 1)
-              last = amt
-            # puts ("amt: #{amt}, total: #{total}")
+  def metric_total(metric_obj)
+    h.number_to_human(metric_obj[:total], precision: 0)
+  end
+  def metric_pct_of_total(metric_obj)
+    amt = metric_obj[:total].to_f
+    total = metrics.first[:total]
+    h.number_to_percentage((amt/total) * 100, precision: 1)
+  end
 
-            h.content_tag(:td, metric_obj[:metric].name) +
-            h.content_tag(:td, h.number_to_human(amt, precision: 0)) +
-            h.content_tag(:td, pc_total) +
-            h.content_tag(:td, pc_from_last)
-          end
-          rows << row
-        end
-        rows.html_safe
-      end
-      head + body
+  def metric_drop_from_last(metric_obj, counter)
+    if counter ==0
+      '--'
+    else
+      last = metrics[counter-1][:total]
+      amt = metric_obj[:total].to_f
+      h.number_to_percentage(((last-amt)/last) * 100, precision: 1)
     end
   end
+
+
+
+
+
+
+
+  private
+
+
 end
-
-def funnel_chart
-  options =
-  {series:
-    { funnel:
-      {
-        show: true,
-
-        margin: { right: 0.15},
-        label: {
-          show: true,
-          align: "center",
-          threshold: 0.05,
-
-          },
-          highlight: {
-            opacity: 0.2
-          }
-          },
-          },
-          grid: {
-            hoverable: true,
-            clickable: true
-          }
-        }
-
-        data = []
-        metrics.each do |metric_obj|
-          data << {label: metric_obj[:metric].name, data:metric_obj[:total] }
-        end
-
-
-        h.content_tag(:div, "", id: 'funnel-chart', style: 'height:800px; width:800px', data: {chart_data: data, chart_options:options})
-      end
-
-
-
-
-
-      private
-
-
-    end
