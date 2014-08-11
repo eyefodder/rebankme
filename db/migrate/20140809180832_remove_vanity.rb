2c5862ab797294c24c4e3108e923a5cb14510624
Dylan Butman <dbutman@nyu.edu>
2014-08-11 17:38:23 -0400

    rm vanity

--- db/migrate/20140809180832_remove_vanity.rb
+++
@@ -1,9 +0,0 @@
-class RemoveVanity < ActiveRecord::Migration
-  def self.up
-    drop_table :vanity_metrics
-    drop_table :vanity_metric_values
-    drop_table :vanity_experiments
-    drop_table :vanity_conversions
-    drop_table :vanity_participants
-  end
-end
