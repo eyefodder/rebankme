5de10e70298232aca07e5fff6d0a8864da088b6b
Paul BH <eyefodder@gmail.com>
2014-08-11 16:00:18 -0400

    test for account finder bug where user params get lost in the post

---
+++ spec/controllers/account_finder_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe AccountFinderController,:type => :controller do
+  # NB most tests are carried out as integration tests
+  # but this one is in response to a bug where following a certain path
+  # results in the user params not getting posted to the controller
+  # which we rescue and redirect to the start path
+  describe '#next_type_question' do
+    it 'redirects to start path if no user params supplied' do
+      post :next_type_question
+      expect(response).to redirect_to(account_finder_start_path)
+    end
+
+  end
+end
